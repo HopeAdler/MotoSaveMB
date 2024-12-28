591f09c59dd94d43c73221b0a08d2752105f6c4c
HopeAdler <buihoangphucthd@gmail.com>
2024-12-28 22:07:34 +0700

    setup ui lib

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
+import { Box } from "@/components/ui/box";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Box className="bg-primary-500 p-10">
+        <Text className="text-typography-0 text-center font-extrabold text-2xl">
+          MotoSave
+        </Text>
+      </Box>
     </View>
   );
 }
