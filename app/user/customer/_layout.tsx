23cd867aa01aaa88836de613cb28f7af8d6c01cb
Bùi Hoàng Phúc <70421426+HopeAdler@users.noreply.github.com>
2025-01-13 11:07:14 +0700

    set up customer layout (#6)
    * add map package
    
    * set up customer layout

--- app/user/customer/_layout.tsx
+++ app/user/customer/_layout.tsx
@@ -1,14 +1,65 @@
-import { Stack } from "expo-router";
-
+import { Tabs } from "expo-router";
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import {
+  House,
+  SquareChartGantt,
+  CreditCard,
+  Bell,
+  CircleUserRound,
+} from "lucide-react-native";
 
 export default function CustomerLayout() {
   return (
     <GluestackUIProvider mode="light">
-      <Stack >
-      <Stack.Screen name="home" options={{ headerShown: false }} />
-      </Stack>
+      <Tabs>
+        <Tabs.Screen
+          name="home"
+          options={{
+            headerShown: false,
+            tabBarLabel: "Home",
+            tabBarIcon: (tabInfo) => <House size={28} color={tabInfo.color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="activity"
+          options={{
+            headerShown: false,
+            tabBarLabel: "Activity",
+            tabBarIcon: (tabInfo) => (
+              <SquareChartGantt size={28} color={tabInfo.color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="payment"
+          options={{
+            headerShown: false,
+            tabBarLabel: "Payment",
+            tabBarIcon: (tabInfo) => (
+              <CreditCard size={28} color={tabInfo.color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="notification"
+          options={{
+            headerShown: false,
+            tabBarLabel: "Notifications",
+            tabBarIcon: (tabInfo) => <Bell size={28} color={tabInfo.color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="account"
+          options={{
+            headerShown: false,
+            tabBarLabel: "Account",
+            tabBarIcon: (tabInfo) => (
+              <CircleUserRound size={28} color={tabInfo.color} />
+            ),
+          }}
+        />
+      </Tabs>
     </GluestackUIProvider>
   );
 }
