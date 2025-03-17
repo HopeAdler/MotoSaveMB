dfa0e45940fc3255975d14259be15a51883c897c
Nadariano <aduy1912@gmail.com>
2025-03-17 11:06:41 +0700

    fix route

--- app/user/customer/home/normalRescue/_layout.tsx
+++ app/user/customer/home/normalRescue/_layout.tsx
@@ -5,7 +5,6 @@ export default function NormalRescuelayout() {
   return (
     <Stack>
       <Stack.Screen name="rescueMap" options={{ headerShown: false }} />
-      <Stack.Screen name="chatScreen" options={{ headerShown: false }} />
     </Stack>
   );
 }
