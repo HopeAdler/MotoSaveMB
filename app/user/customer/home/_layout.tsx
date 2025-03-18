67ecaae4e92b7567870b0edd0c8860f682ac18da
ductain <taindse170080@fpt.edu.vn>
2025-03-18 12:14:02 +0700

    add craete emergency request

--- app/user/customer/home/_layout.tsx
+++ app/user/customer/home/_layout.tsx
@@ -9,6 +9,7 @@ export default function Homelayout() {
       <Stack.Screen name="feedback" options={{ headerShown: false }} />
       <Stack.Screen name="normalRescue" options={{ headerShown: false }} />
       <Stack.Screen name="floodRescue" options={{ headerShown: false }} />
+      <Stack.Screen name="emergencyRescue" options={{ headerShown: false }} />
       <Stack.Screen name="chatScreen" options={{ headerShown: false }} />
     </Stack>
   );
