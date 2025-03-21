e31616cf3460e80194031f2be664c672a2959681
Nadariano <aduy1912@gmail.com>
2025-03-21 17:45:51 +0700

    add mechanic screens v0.5

---
+++ app/user/mechanic/requests/chatScreen.tsx
@@ -0,0 +1,26 @@
+import ChatViewComponent from "@/components/custom/ChatViewComponent";
+import { GoBackButton } from "@/components/custom/GoBackButton";
+import { Box } from "@/components/ui/box";
+import { useLocalSearchParams } from "expo-router";
+import { Text } from "react-native";
+
+const ChatScreen = () => {
+    const { currentUserId, requestDetailId } = useLocalSearchParams();
+
+    if (!currentUserId || !requestDetailId) {
+        return null; // Prevent rendering if any parameter is missing
+    }
+
+    return (
+        <Box className="flex-1 p-5">
+            <GoBackButton/>
+            <Text>Chat với khách hàng</Text>
+            <ChatViewComponent
+                currentUserId={currentUserId as string}
+                requestDetailId={requestDetailId as string}
+            />
+        </Box>
+    );
+}
+
+export default ChatScreen;
