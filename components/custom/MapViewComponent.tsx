c8607cce980a55ea2b41c48e42661156fb13f54a
Nadariano <aduy1912@gmail.com>
2025-04-02 11:54:19 +0700

    fix camera zoom in driver

--- components/custom/MapViewComponent.tsx
+++ components/custom/MapViewComponent.tsx
@@ -1,24 +1,21 @@
 import React, { useEffect, useRef } from "react";
 import MapboxGL from "@rnmapbox/maps";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { View } from "react-native";
 import UserMarker from "./UserMarker";
-import { LocateFixed } from "lucide-react-native";
-import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 import MyLocationButton from "./MyLocationButton";
 import { useSharedValue, withTiming, useAnimatedStyle } from "react-native-reanimated";
 import { User } from "../../app/context/formFields";
-
 
 type Users = Map<string, User>;
 
 const { EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN } = process.env;
 const { EXPO_PUBLIC_GOONG_MAP_KEY } = process.env;
-// const [showActionsheet, setShowActionsheet] = useState(false);
+
 type MapViewComponentProps = {
   users: Users;
   currentLoc: { latitude: number; longitude: number };
   focusMode: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
-  children?: React.ReactNode | undefined; // Accepts any JSX elements
+  children?: React.ReactNode;
   isActionSheetOpen: boolean;
 };
 
@@ -26,64 +23,49 @@ const MapViewComponent: React.FC<MapViewComponentProps> = ({
   users,
   currentLoc,
   focusMode: [focusOnMe, setFocusOnMe],
-  children, // Add children here
-  isActionSheetOpen
+  children,
+  isActionSheetOpen,
 }) => {
   const loadMap = `https://tiles.goong.io/assets/goong_map_web.json?api_key=${EXPO_PUBLIC_GOONG_MAP_KEY}`;
   const mapRef = useRef<MapboxGL.MapView>(null);
   const cameraRef = useRef<MapboxGL.Camera>(null);
-  const focusLoc = () => {
-    setFocusOnMe(!focusOnMe);
+
+  MapboxGL.setAccessToken(`${EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN}`);
+
+  useEffect(() => {
     if (focusOnMe && cameraRef.current) {
       cameraRef.current.setCamera({
         centerCoordinate: [currentLoc.longitude, currentLoc.latitude],
         zoomLevel: 14,
-        animationDuration: 2000,
+        animationDuration: 1000,
       });
     }
-    console.log("Current Users: " + users.size);
-  };
+  }, [focusOnMe, currentLoc]);
 
-  MapboxGL.setAccessToken(`${EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN}`);
   const locationButtonOffset = useSharedValue(10);
   useEffect(() => {
     locationButtonOffset.value = withTiming(isActionSheetOpen ? 150 : 10, { duration: 300 });
   }, [isActionSheetOpen]);
-  const animatedButtonStyle = useAnimatedStyle(() => ({ bottom: locationButtonOffset.value }));
+
+  const animatedButtonStyle = useAnimatedStyle(() => ({
+    bottom: locationButtonOffset.value,
+  }));
+
   return (
-    <View className="flex-1" >
+    <View className="flex-1">
       <MapboxGL.MapView styleURL={loadMap} ref={mapRef} style={{ flex: 1 }}>
-        <MapboxGL.Camera
-          ref={cameraRef}
-          zoomLevel={14}
-        // centerCoordinate={[currentLoc.longitude, currentLoc.latitude]}
-        />
-        {
-          Array.from(users.entries()).map(([key, user]) => (
-            <UserMarker key={key} user={user} />
-          ))
-        }
-        {children /* Render additional components passed from parent */}
+        <MapboxGL.Camera ref={cameraRef} zoomLevel={14} />
+
+        {Array.from(users.entries()).map(([key, user]) => (
+          <UserMarker key={key} user={user} />
+        ))}
+
+        {children}
       </MapboxGL.MapView>
-      <View >
-        {/* <<TouchableOpacity onPress={focusLoc}>
-          <LocateFixed color="#0080FF" size={28} />
-        </TouchableOpacity>> */}
-        <MyLocationButton onPress={focusLoc} isActionSheetOpen={isActionSheetOpen} />
-      </View>
+
+      <MyLocationButton onPress={() => setFocusOnMe(!focusOnMe)} isActionSheetOpen={isActionSheetOpen} />
     </View>
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: { flex: 1 },
-//   map: { flex: 1 },
-//   bottom: {
-//     position: "absolute",
-//     bottom: hp("4%"),
-//     alignSelf: "center",
-//   },
-// });
-
 export default MapViewComponent;
-
